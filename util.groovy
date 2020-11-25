884f50b360bc6330e496ef7757d78fbbc5423569
Shawn S Lin <shawn_s_lin@trendmicro>
2020-11-25 17:18:31 +0800

    test

--- util.groovy
+++ util.groovy
@@ -6,4 +6,16 @@ def clean(){
     sh 'clean'
 }
 
+def call(Map<String, String> pipelineConfig) {
+    pipeline{
+        agent any
+
+        stages{
+           stage('Update repository'){
+               sh 'remote file'
+           }
+        }
+    }
+}
+
 return this;
